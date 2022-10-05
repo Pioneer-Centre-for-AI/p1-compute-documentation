a068ac860f38044aacda5ad8404b0f67ded03494
A. B. Sørensen <anders-soerensen@hotmail.com>
2022-10-05 15:26:33 +0200

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,12 +2,12 @@
 
 # -- Project information
 
-project = 'documentation'
-copyright = '2022, Pioneer Centre for AI'
-author = 'Pioneer Centre for AI'
+project = 'Lumache'
+copyright = '2021, Graziella'
+author = 'Graziella'
 
-release = '0.2'
-version = '0.2.0'
+release = '0.1'
+version = '0.1.0'
 
 # -- General configuration
 
